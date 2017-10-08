44af80240c4ba2e6d9a2f660b31ddfbc1c743dac
Matthew Casperson <matthewcasperson@gmail.com>
2017-10-09 07:38:01 +1000

    Fixing executable driver

--- src/test/java/firefox/Test.java
+++ src/test/java/firefox/Test.java
@@ -4,10 +4,13 @@ import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
+import java.io.File;
+
 public class Test {
     @org.junit.Test
     public void FirefoxHeadlessTest() {
         final String geckodriver = this.getClass().getResource("/geckodriver").getFile();
+        new File(geckodriver).setExecutable(true);
         System.setProperty("webdriver.gecko.driver", geckodriver);
 
         final FirefoxBinary firefoxBinary = new FirefoxBinary();
